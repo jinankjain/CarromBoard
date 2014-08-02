b83c7a4d2c58c8002e660945facc0114f0f20c88
Jinank Jain <ug201210017@iitj.ac.in>
2014-08-02 22:11:03 +0530

    Basic GUI Completed

--- carrom.py
+++ carrom.py
@@ -21,7 +21,26 @@ class CarromBoard():
 
 	def draw(self):
 		pygame.draw.rect(self.screen,wooden,(0,0,self.width,self.height),50)
-		#pygame.draw.rect(self.screen,black,(0,0,20,20),50)
+		
+		pygame.draw.lines(self.screen, black, False, [(160,100),(830,100)],2)
+		pygame.draw.lines(self.screen, black, False, [(160,140),(830,140)],2)
+		pygame.draw.lines(self.screen, black, False, [(160,860),(830,860)],2)
+		pygame.draw.lines(self.screen, black, False, [(160,900),(830,900)],2)
+		pygame.draw.circle(self.screen, black, (160,120), 20)
+		pygame.draw.circle(self.screen, black, (160,880), 20)
+		pygame.draw.circle(self.screen, black, (830,120), 20)
+		pygame.draw.circle(self.screen, black, (830,880), 20)
+
+
+		pygame.draw.lines(self.screen, black, False, [(100,160),(100,830)],2)
+		pygame.draw.lines(self.screen, black, False, [(140,160),(140,830)],2)
+		pygame.draw.lines(self.screen, black, False, [(900,160),(900,830)],2)
+		pygame.draw.lines(self.screen, black, False, [(860,160),(860,830)],2)
+		pygame.draw.circle(self.screen, black, (120,160), 20)
+		pygame.draw.circle(self.screen, black, (120,830), 20)
+		pygame.draw.circle(self.screen, black, (880,160), 20)
+		pygame.draw.circle(self.screen, black, (880,830), 20)
+		
 		pygame.draw.circle(self.screen, black, (47,47), 30)
 		pygame.draw.circle(self.screen, black, (953,47), 30)
 		pygame.draw.circle(self.screen, black, (47,953), 30)
@@ -32,13 +51,19 @@ class CarromBoard():
 		pygame.draw.circle(self.screen, black, (587,550), 25)
 		pygame.draw.circle(self.screen, yellow2, (543,575), 25)
 		pygame.draw.circle(self.screen, yellow2, (457,575), 25)
-		pygame.draw.circle(self.screen, yellow2, (413,575), 25)
+		pygame.draw.circle(self.screen, black, (414,552), 25)
 		pygame.draw.circle(self.screen, yellow2, (457,425), 25)
 		pygame.draw.circle(self.screen, black, (500,600), 25)
 		pygame.draw.circle(self.screen, yellow2, (457,525), 25)
 		pygame.draw.circle(self.screen, black, (457,475), 25)
 		pygame.draw.circle(self.screen, black, (543,475), 25)
 		pygame.draw.circle(self.screen, yellow2, (500,450), 25)
+		pygame.draw.circle(self.screen, yellow2, (410,500), 25)
+		pygame.draw.circle(self.screen, yellow2, (590,500), 25)
+		pygame.draw.circle(self.screen, black, (412,450), 25)
+		pygame.draw.circle(self.screen, black, (585,450), 25)
+		pygame.draw.circle(self.screen, yellow2, (542,425), 25)
+		pygame.draw.circle(self.screen, black, (500,400), 25)
 
 	def run(self):
 		while True:
