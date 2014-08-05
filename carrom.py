bc234432cfb7804fee3dee5318bf90f5bb9745d6
Jinank Jain <ug201210017@iitj.ac.in>
2014-08-05 10:28:51 +0530

    Striker direction added

--- carrom.py
+++ carrom.py
@@ -9,6 +9,8 @@ yellow=(255,204,154)
 black = (0,0,0)
 pink = (242,4,105)
 yellow2 = (238,199,94)
+clock = pygame.time.Clock()
+green = (102,205,0)
 
 class Goti(pygame.sprite.Sprite):
 	def __init__(self,color,radius,x,y):
@@ -20,6 +22,32 @@ class Goti(pygame.sprite.Sprite):
 		self.rect.x = x-radius
 		self.rect.y = y-radius
 		pygame.draw.circle(self.image, color,(radius,radius),radius)
+		pygame.draw.circle(self.image, black,(radius,radius),radius,2)
+
+class Striker():
+	def __init__(self,screen):
+		self.striker_list = pygame.sprite.Group()
+		self.striker = Goti(blue,28,495,880)
+		self.striker_list.add(self.striker)
+		self.screen = screen
+
+	def update(self,state):
+		pos = pygame.mouse.get_pos()
+		if(state==0):
+	 		if(pos[0]<168):
+	 			self.striker.rect.centerx = 168
+	 		elif(pos[0]>822):
+	 			self.striker.rect.centerx = 822
+	 		else:
+	 			self.striker.rect.centerx = pos[0]
+	 	self.striker_list.draw(self.screen)
+			
+	def directStriker(self):
+		pos = pygame.mouse.get_pos()
+		if(pos[1]>860):
+			pygame.draw.lines(self.screen,red, False,[(self.striker.rect.centerx,self.striker.rect.centery),(pos[0],pos[1])],2)
+		else:
+			pygame.draw.lines(self.screen,green, False,[(self.striker.rect.centerx,self.striker.rect.centery),(pos[0],pos[1])],2)
 
 class CarromBoard():
 	def __init__(self, width=1000, height=1000, caption="Carrom Board"):
@@ -27,32 +55,43 @@ class CarromBoard():
 		self.width, self.height, self.caption = width, height, caption
 		self.screen=pygame.display.set_mode((self.width, self.height))
 	 	pygame.display.set_caption(self.caption)
-	 	
+	 	self.striker = Striker(self.screen)
 		self.draw()
 
 	def draw(self):
 		self.screen.fill(yellow)
+		#side border
 		pygame.draw.rect(self.screen,wooden,(0,0,self.width,self.height),50)
 		
+		#up and down lines and circles
 		pygame.draw.lines(self.screen, black, False, [(160,100),(830,100)],2)
 		pygame.draw.lines(self.screen, black, False, [(160,140),(830,140)],2)
 		pygame.draw.lines(self.screen, black, False, [(160,860),(830,860)],2)
 		pygame.draw.lines(self.screen, black, False, [(160,900),(830,900)],2)
 		pygame.draw.circle(self.screen, red, (160,120), 20)
+		pygame.draw.circle(self.screen, black, (160,120), 20,2)
 		pygame.draw.circle(self.screen, red, (160,880), 20)
+		pygame.draw.circle(self.screen, black, (160,880), 20,2)
 		pygame.draw.circle(self.screen, red, (830,120), 20)
+		pygame.draw.circle(self.screen, black, (830,120), 20,2)
 		pygame.draw.circle(self.screen, red, (830,880), 20)
+		pygame.draw.circle(self.screen, black, (830,880), 20,2)		
 
-
+		#left and right lines and circles
 		pygame.draw.lines(self.screen, black, False, [(100,160),(100,830)],2)
 		pygame.draw.lines(self.screen, black, False, [(140,160),(140,830)],2)
 		pygame.draw.lines(self.screen, black, False, [(900,160),(900,830)],2)
 		pygame.draw.lines(self.screen, black, False, [(860,160),(860,830)],2)
 		pygame.draw.circle(self.screen, red, (120,160), 20)
+		pygame.draw.circle(self.screen, black, (120,160), 20,2)
 		pygame.draw.circle(self.screen, red, (120,830), 20)
+		pygame.draw.circle(self.screen, black, (120,830), 20,2)
 		pygame.draw.circle(self.screen, red, (880,160), 20)
+		pygame.draw.circle(self.screen, black, (880,160), 20,2)
 		pygame.draw.circle(self.screen, red, (880,830), 20)
+		pygame.draw.circle(self.screen, black, (880,830), 20,2)
 		
+		#four holes position at the border
 		pygame.draw.circle(self.screen, black, (47,47), 30)
 		pygame.draw.circle(self.screen, black, (953,47), 30)
 		pygame.draw.circle(self.screen, black, (47,953), 30)
@@ -80,29 +119,27 @@ class CarromBoard():
 		goti_list.add(Goti(yellow2,25,543,525))
 		goti_list.add(Goti(pink,25,500,500))
 		goti_list.draw(self.screen)
+		
 
-		self.striker_list = pygame.sprite.Group()
-		self.striker = Goti(blue,28,495,880)
-		self.striker_list.add(self.striker)
 		
 	def run(self):
+		state = 0
 		while True:
 	 		for event in pygame.event.get():
 	 			if event.type==QUIT:
 	 				pygame.quit()
 	 				sys.exit()
+	 			if event.type==MOUSEBUTTONDOWN:
+	 				state=1
+	 				mouse_src = pygame.mouse.get_pos()
+
+	 			self.striker.update(state)
 	 			pygame.display.update()
 	 		self.draw()
-	 		pos = pygame.mouse.get_pos()
-	 		print pos[0]
-	 		if(pos[0]<140):
-	 			self.striker.rect.x = 140
-	 		elif(pos[0]>800):
-	 			self.striker.rect.x = 800
-	 		else:
-	 			self.striker.rect.x = pos[0]
-			self.striker_list.draw(self.screen)
-	 		pygame.display.flip()
+	 		if(state==1):
+	 			self.striker.directStriker()
+	 		clock.tick(20)
+	 		#pygame.display.flip()
 
 
 def main():
